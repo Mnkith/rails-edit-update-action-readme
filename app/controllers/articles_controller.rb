a4b0ac010c5848e01e36b0ab861acbdceb02ba98
Mnkith <munkithabid@gmail.com>
2021-02-25 18:01:45 -0600

    commit tree 1c12b61c15a4f5e32bc84911d50fef69bc2b6106

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,18 +6,26 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
 
   def new
     @article = Article.new
   end
 
   def create
-    @article = Article.new
-    @article.title = params[:title]
-    @article.description = params[:description]
-    @article.save
-    redirect_to article_path(@article)
+    article = Article.new
+    article.title = params[:title]
+    article.description = params[:description]
+    article.save
+    redirect_to article_path(article)
   end
 
-  # add edit and update methods here
+  def update
+    # raise params.inspect
+    a = Article.find params[:id]
+    a.update params.require(:article).permit(:title, :description)
+    redirect_to article_path(a)
+  end 
 end
